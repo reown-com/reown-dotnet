859d88695f3291788ba0e57a523abbd02fbc7d59
skibitsky <gleb@skibitsky.com>
2025-09-12 14:22:59 +0300

    Disable embedded wallet in solana sdk sample

--- playground/Reown.SolanaSdk.Unity/Assets/Scripts/Main.cs
+++ playground/Reown.SolanaSdk.Unity/Assets/Scripts/Main.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Reown.AppKit.Unity;
 using Reown.Core.Common.Logging;
@@ -14,32 +15,34 @@ public class Main : MonoBehaviour
     {
         // Set up Reown logger to collect logs from AppKit
         ReownLogger.Instance = new UnityLogger();
-        
+
         // Create AppKit config object
         var config = new AppKitConfig
         {
             // Project ID from https://cloud.reown.com/
             projectId = "610570e13c15bf6e35d12aa2ba945100", // don't use this project id in prod!
             metadata = new Metadata(
-                name: "Solana Sdk",
-                description: "Testing Solana.Unity-Sdk + Reown AppKit",
-                url: "https://reown.com",
-                iconUrl: "https://raw.githubusercontent.com/reown-com/reown-dotnet/refs/heads/develop/sample/Reown.AppKit.Unity/Assets/Textures/appkit-icon-unity.png",
+                "Solana Sdk",
+                "Testing Solana.Unity-Sdk + Reown AppKit",
+                "https://reown.com",
+                "https://raw.githubusercontent.com/reown-com/reown-dotnet/refs/heads/develop/sample/Reown.AppKit.Unity/Assets/Textures/appkit-icon-unity.png",
                 new RedirectData
                 {
                     Native = "solanasdktest://"
                 }
             ),
+            enableEmail = false,
+            socials = Array.Empty<SocialLogin>(),
             supportedChains = new[]
             {
                 ChainConstants.Chains.Solana,
-                ChainConstants.Chains.SolanaDevNet, // note, that few mobile wallets support devnet
+                ChainConstants.Chains.SolanaDevNet // note, that few mobile wallets support devnet
             }
         };
-        
+
         // Initialize AppKit with config
         await AppKit.InitializeAsync(config);
-        
+
         // Try to resume AppKit session
         var (resumed, _) = await _web3.TryResumeAppKitSession();
         Debug.Log($"AppKit session resumed: {resumed}");
